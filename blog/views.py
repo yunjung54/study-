0b0e9bf484553a8a2e810bd5d681abe3ad071523
yunjung54 <djawlrkfkr@naver.com>
2019-07-11 14:31:39 +0900

    어드민블로그

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from django.contrib import admin
+from  .models import Blog
 
+admin.site.register(Blog) #블로그 형식을 가져와 등록하겠다.
 # Create your views here.
 def home(request):
-  return render(request,'home.html')
+  blogs = Blog.objects
+  return render(request,'home.html',{'blogs':blogs})
+
